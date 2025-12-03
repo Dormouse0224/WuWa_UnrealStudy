79e547352e33ded250a5e001202c97ede7dc80c3
Dormouse <arcticpenguin@naver.com>
2025-12-03 18:27:47 +0900

    1. 불필요한 중복 파일 제거 2. RibbonAnim을 애디티브로 설정

---
+++ Source/WutheringWaves/AnimNode/ApplyAdditive_Rib.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class WUTHERINGWAVES_API ApplyAdditive_Rib
+{
+public:
+	ApplyAdditive_Rib();
+	~ApplyAdditive_Rib();
+};
